7202f6fecc7dce3a691a480e77f231c0ebdfc4f9
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-01-22 23:21:34 -0500

    Added basic routin, register & login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Container } from './components/Utils';
 import AppBar from './components/AppBar';
 import Footer from './components/Footer';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <div>
       <AppBar />
       <Container ___class="app">
-        
+        <Route exact path="/" component={props => <Register {...props} />} />
+        <Route path="/login" component={props => <Login {...props} />} />
       </Container>
       <Footer />
     </div>
---
+++ client/src/components/Register.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Register = props => {
+    return <div></div>
+}
+
+export default Register;
